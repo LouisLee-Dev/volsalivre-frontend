79c7e85e845179fcfe37577e634dbee0889e996b
LouisLee-Dev <louislee0996@gmail.com>
2024-08-01 06:35:32 -0700

    fix:router fixed

--- components/artigos/header/page.tsx
+++ components/artigos/header/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Logo from "@/public/assets/images/logo_blue.png";
-import Login from "@/components/artigos/login";
+import Login from "@/components/header/login";
 import Menu from "@/components/artigos/header/menu";
 import { MenuTipCard } from "@/components/basecomponents/cards";
 
